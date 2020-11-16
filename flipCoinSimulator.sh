8a8158071e4b5e107c787963bccc52f6bc4da890
purushothaman-source <purushoth99jun@gmail.com>
2020-11-16 19:13:25 +0530

    ADDED:print heads and tails won times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,17 @@
 #! /bin/bash 
+num=0
+heads=0
+tails=0
+while (( num <100 ))
+do
+((num++))
 random=$(( RANDOM%2 ))
 if (( $random == 1))
 then
-  echo "head"
+  ((heads++))
 else
-  echo "tail"
+  ((tails++))
 fi
+done
+echo "head won $heads times"
+echo "tail won $tails times"
